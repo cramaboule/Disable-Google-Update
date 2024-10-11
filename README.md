86437662a14c12effa8347ad57f75ade93a6d573
cramaboule <cramaboule@gmail.com>
2024-10-11 12:05:13 +0200

    fix bug and delete services

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-Simple script to Disable All Google Updater (GoogleUpdater*XXX.X.XXXX.X)
+Simple script to Disable All Google Updater and Detete service (GoogleUpdater*XXX.X.XXXX.X) and Gupdate*
+Open RPC port 135
+and
+Remote Service Management (RPC): RPC Dynamic Ports
--- diable_gupdate.bat
+++ diable_gupdate.bat
@@ -1,21 +1,27 @@
 ::Disable Google Update on all server
-
 @echo off
 
 if not "%1"=="am_admin" (powershell start -verb runas '%0' am_admin & exit /b)
 cd %~dp0
 
+SET ListServer=C:\path\to\Serveurs-list.txt
+
+echo %DATE% %TIME% > GUpdate.log
 
 SETLOCAL ENABLEDELAYEDEXPANSION
 
-for /f "tokens=*" %%a in (C:\path\to\Serveurs-list.txt) do (
-	echo %%a
+for /f "tokens=*" %%a in (%ListServer%) do (
+	echo %%a | tee GUpdate.log
 	sc \\%%a query state=all | findstr /I /R /C:"^SERVICE_NAME: Googleu.*" > GUpdate.txt
-	SET count=1
 	FOR /F "tokens=1,2" %%G IN (GUpdate.txt) DO (
-	  echo %%H
-	  SC \\%%a config %%H start= disabled
+	  echo %%H | tee GUpdate.log
+	  SC \\%%a config %%H start= disabled | tee GUpdate.log
+	  SC \\%%a DELETE %%H | tee GUpdate.log
 	)
-	SC \\%%a config gupdate start= disabled
-
+	sc \\%%a query state=all | findstr /I /R /C:"^SERVICE_NAME: GUpdate" > GUpdate.txt
+	FOR /F "tokens=1,2" %%G IN (GUpdate.txt) DO (
+	  echo %%H | tee GUpdate.log
+	  SC \\%%a config %%H start= disabled | tee GUpdate.log
+	  SC \\%%a DELETE %%H | tee GUpdate.log
+	)
 )
